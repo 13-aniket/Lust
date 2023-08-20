3daae8eaa744e31f81ca16438ef9a6d475502605
Aniket Niranjan <71547724+13-aniket@users.noreply.github.com>
2023-08-20 19:19:33 +0530

    Update and rename lust.py to SeductiveAlgorithm.py

---
+++ SeductiveAlgorithm.py
@@ -0,0 +1,5 @@
+# Seductive Algorithm
+# This algorithm entices with its complexity, alluring the curious minds into its embrace.
+
+def seduce():
+    print("You are captivated by the beauty of the algorithm.")
